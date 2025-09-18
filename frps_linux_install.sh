5bbbe402d73607c48d717aefb52e3dc61459a17a
胡说丷刂 <34758853+laosan-xx@users.noreply.github.com>
2025-09-19 01:07:00 +0800

    Update frps_linux_install.sh

--- frps_linux_install.sh
+++ frps_linux_install.sh
@@ -74,15 +74,15 @@ FILE_NAME=frp_${FRP_VERSION}_linux_${PLATFORM}
 
 if [ ! -f "${WORK_PATH}/${FILE_NAME}.tar.gz" ]; then
     if [ $GOOGLE_HTTP_CODE == "200" ]; then
-        wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
+        wget -P ${WORK_PATH} https://github.com/laosan-xx/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
         wget -P ${WORK_PATH} https://raw.githubusercontent.com/${REPO}/master/${FRP_NAME}.toml -O ${FRP_NAME}.toml
     else
         if [ $PROXY_HTTP_CODE == "200" ]; then
-            wget -P ${WORK_PATH} ${PROXY_URL}https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
+            wget -P ${WORK_PATH} ${PROXY_URL}https://github.com/laosan-xx/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
             wget -P ${WORK_PATH} ${PROXY_URL}https://raw.githubusercontent.com/${REPO}/master/${FRP_NAME}.toml -O ${FRP_NAME}.toml
         else
             echo -e "${Red}检测 GitHub Proxy 代理失效 开始使用官方地址下载${Font}"
-            wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
+            wget -P ${WORK_PATH} https://github.com/laosan-xx/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
             wget -P ${WORK_PATH} https://raw.githubusercontent.com/${REPO}/master/${FRP_NAME}.toml -O ${FRP_NAME}.toml
         fi
     fi
